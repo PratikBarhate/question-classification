d94c65d7c780a8e452b0a8f82e788ce04aab2d52
PratikNarhar Barhate <pratiknarhar.barhate@kohls.com>
2018-02-02 23:44:35 +0530

    Coarse - NLP partial complete, without NER. 1. NER for spacy is not working good. 2. Process to add doc container from spaCy lib. 3. Save the doc container for train and test data in parallel threads.

---
+++ nlp/__init__.py
@@ -0,0 +1,55 @@
+import pre_processing
+from utils.file_ops import write_obj
+from nlp.proc_coarse import com_annotations
+from multiprocessing.pool import ThreadPool
+import datetime
+
+
+def coarse_computations(data_type: str):
+    """
+    :argument:
+        :param data_type: String either `training` or `test`
+    :return:
+        boolean_flag: True for successful operation.
+    """
+    data = "training" if data_type == "training" else "test"
+    doc_flag, doc_annot = com_annotations(data)
+    if doc_flag:
+        doc_w_flag = write_obj(doc_annot, "coarse_nlp_{0}_doc".format(data))
+        if doc_w_flag:
+            print("- Computing annotations for {0} data done.".format(data))
+            return True
+        else:
+            print("\n- ERROR: While writing annotations for {0} data.".format(data))
+            return False
+    else:
+        print("\n- ERROR: While computing annotations for {0} data.".format(data))
+        return False
+
+
+def main():
+    # ensures pre_processing in done
+    pre_processing.main()
+    # start timer
+    start = datetime.datetime.now().timestamp()
+    print("\n* NLP for coarse classes")
+    # Create two threads for processing training and test raw files
+    pool = ThreadPool(processes=2)
+    # start the threads and wait for them to finish
+    train_result = pool.apply_async(coarse_computations, args=["training"])
+    test_result = pool.apply_async(coarse_computations, args=["test"])
+    train_val = train_result.get()
+    test_val = test_result.get()
+    if not train_val:
+        print("- Error: In computing annotations for training data")
+    if not test_val:
+        print("- Error: In computing annotations for test data")
+    if train_val and test_val:
+        # timer for end time
+        end = datetime.datetime.now().timestamp()
+        total = datetime.datetime.utcfromtimestamp(end - start)
+        print("- NLP : Annotations done in {0}h {1}m {2}s".format(total.hour, total.minute, total.second))
+
+
+if __name__ == '__main__':
+    main()
---
+++ nlp/proc_coarse.py
@@ -0,0 +1,29 @@
+import spacy
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+from utils.file_ops import read_file, read_key
+
+
+def com_annotations(data_type: str):
+    """
+    Function gets all the annotations, Lemma, POS, IS_STOPWORD etc., in the form of spaCy doc container for all the
+    rows (lines) in the text data and saves the object of list of the doc (container) for each line.
+    # doc = spaCy container - [https://spacy.io/api/doc]
+
+    :argument:
+        :param data_type: String either `training` or `test`
+    :return:
+        boolean_flag: True for successful operation.
+        all_annotations: List of doc (spaCy containers) of all the lines in the data.
+    """
+    nlp = spacy.load("en_core_web_lg")
+    all_annotations = []
+    read_flag, file = read_file("raw_sentence_{0}".format(data_type))
+    if read_flag:
+        for line in file:
+            doc = nlp(line)
+            all_annotations.append(doc)
+        file.close()
+        return True, all_annotations
+    else:
+        return False
